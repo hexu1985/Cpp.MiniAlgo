a6360ff41c5df1f9d8de92e52b410ae80d77b5f1
hexu <hexu_1985@sina.com>
2021-08-23 20:22:30 +0800

    add dense_graph/recipe-01

---
+++ code/graph/dense_graph/recipe-01/include/graph_io.hpp
@@ -0,0 +1,41 @@
+/**
+ * @file dense_graph_io.hpp
+ * @brief 稠密图的输入输出运算符重载
+ * @author hexu_1985@sina.com
+ * @version 1.0
+ * @date 2020-05-19
+ */
+#ifndef MINI_ALGO_DENSE_GRAPH_IO_INC
+#define MINI_ALGO_DENSE_GRAPH_IO_INC
+
+#include <cmath>
+#include <iostream>
+#include <iomanip>
+
+namespace mini_algo {
+
+/**
+ * @brief 稠密图的输出运算符重载
+ *
+ * @param strm 输出流
+ * @param graph 稠密图
+ *
+ * @return 输出流
+ */
+template <typename charT, typename traits, typename Graph>
+std::basic_ostream<charT, traits>& operator<< (std::basic_ostream<charT, traits>& strm, const Graph& graph)
+{
+    int width = static_cast<int>(log10(graph.V()))+1;
+    for (int v = 0; v < graph.V(); v++) {
+        strm << std::setw(width) << v << ": ";
+        for (auto w: graph.AdjList(v)) {
+            strm << w << ", ";
+        }
+        strm << std::endl;
+    }
+    return strm;
+}
+
+}   // namespace unweight
+
+#endif  // UNWEIGHT_DENSE_GRAPH_IO_INC
