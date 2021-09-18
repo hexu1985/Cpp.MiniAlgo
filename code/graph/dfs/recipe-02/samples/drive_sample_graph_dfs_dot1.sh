e6a0410971126166cd10b64b9f6ea9c9924f6832
hexu <hexu_1985@sina.com>
2021-09-18 11:04:37 +0800

    add graph_dot_stree.hpp

--- code/graph/dfs/recipe-02/samples/drive_sample_graph_dfs_dot1.sh
+++ code/graph/dfs/recipe-02/samples/drive_sample_graph_dfs_dot1.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-echo "./sample_graph_dfs_dot1 graph_dfs.dot"
-./sample_graph_dfs_dot1 graph_dfs.dot
-dot graph_dfs.dot -T png -o graph_dfs.png
-echo "output png is graph_dfs.png"
+echo "./sample_graph_dfs_dot1 graph_dfs1.dot"
+./sample_graph_dfs_dot1 graph_dfs1.dot
+dot graph_dfs1.dot -T png -o graph_dfs1.png
+echo "output png is graph_dfs1.png"
 
