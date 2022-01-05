9153dca40f0487a2dda6d43c471001fe05498669
hexu <hexu_1985@sina.com>
2022-01-05 17:44:17 +0800

    删除vector版本的selection_sort

--- code/sort/selection_sort/recipe-01/include/selection_sort.hpp
+++ code/sort/selection_sort/recipe-01/include/selection_sort.hpp
@@ -26,67 +26,29 @@ namespace mini_algo {
 template <typename T>
 void SelectionSort(T arr[], int n)
 {
-    int smallIndex; // 子表中最小元素的下标
+    int smallIndex; // index of smallest element in the sublist
     int pass, j;
     T temp;
 
-    // pass的范围是0 ~ n-2
+    // pass has the range 0 to n-2
     for (pass = 0; pass < n-1; pass++)
     {
-        // 从下标pass开始扫描子表
+        // scan the sublist starting at index pass
         smallIndex = pass;
 
-        // j遍历子表arr[pass+1]到arr[n-1]
-        for (j = pass+1; j < n; j++) {
-            // 如果找到更小的元素, 则将smallIndex赋值给该位置
-            if (arr[j] < arr[smallIndex]) {
+        // j traverses the sublist arr[pass+1] to arr[n-1]
+        for (j = pass+1; j < n; j++)
+            // update if smaller element found
+            if (arr[j] < arr[smallIndex])
                 smallIndex = j;
-            }
-        }
 
-        // 如果smallIndex和pass不在相同的位置,
-        // 将子表中的最小项与arr[pass]交换
+        // if smallIndex and pass are not the same location,
+        // exchange the smallest item in the sublist with arr[pass]
         if (smallIndex != pass)
         {
             temp = arr[pass];
             arr[pass] = arr[smallIndex];
             arr[smallIndex] = temp;
-        }
-    }
-}
-
-/**
- * @brief 选择排序指定向量
- *
- * @tparam T 向量元素类型
- * @param v  向量引用
- */
-template <typename T>
-void SelectionSort(std::vector<T>& v)
-{
-    int smallIndex; // 子表中最小元素的下标
-    int pass, j, n = v.size();
-    T temp;
-
-    // pass的范围是0 ~ n-2
-    for (pass = 0; pass < n-1; pass++) {
-        // 从下标pass开始扫描子表
-        smallIndex = pass;
-
-        // j遍历子表v[pass+1]到v[n-1]
-        for (j = pass+1; j < n; j++) {
-            // 如果找到更小的元素, 则将smallIndex赋值给该位置
-            if (v[j] < v[smallIndex]) {
-                smallIndex = j;
-            }
-        }
-
-        // 如果smallIndex和pass不在相同的位置,
-        // 将子表中的最小项与arr[pass]交换
-        if (smallIndex != pass) {
-            temp = v[pass];
-            v[pass] = v[smallIndex];
-            v[smallIndex] = temp;
         }
     }
 }
